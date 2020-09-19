7726804067afb3f42bdab954c4a9dc7cd74ca9f5
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-19 13:44:34 +0700

    Create tambah_mahasiswa.php

---
+++ pages/mahasiswa/tambah_mahasiswa.php
@@ -0,0 +1,15 @@
+<div class="content-wrapper">
+    <section class="content-header">
+        <h1>Tambah Mahasiswa</h1>
+        <ol class="breadcrumb">
+            <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Tambah Mahasiswa</li>
+        </ol>
+    </section>
+
+    <section class="content">
+        <div class="row">
+            
+        </div>
+    </section>
+</div>
