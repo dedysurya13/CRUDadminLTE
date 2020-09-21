ebf0334cd0cb99dab48f8f2b8e88c8c75ad1f8c8
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-21 09:43:04 +0700

    tambah mahasiswa

--- pages/mahasiswa/tambah_mahasiswa.php
+++ pages/mahasiswa/tambah_mahasiswa.php
@@ -9,7 +9,46 @@
 
     <section class="content">
         <div class="row">
-            
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <form role="form" method="post" action="pages/mahasiswa/tambah_mahasiswa_proses.php">
+                        <div class="box-body">
+                            <div class="form-group">
+                                <label for="">NIM</label>
+                                <input type="text" name="nim" class="form-control" placeholder="NIM" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Nama</label>
+                                <input type="text" name="nama" class="form-control" placeholder="Nama" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Kelas</label>
+                                <select name="kelas" class="form-control" id="">
+                                    <option value="">- Pilih Kelas -</option>
+                                    <option value="Pagi">Pagi</option>
+                                    <option value="Siang">Siang</option>
+                                    <option value="Malam">Malam</option>
+                                    <option value="Karyawan">Karyawan</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Jurusan</label>
+                                <select name="jurusan" class="form-control" id="">
+                                    <option value="">- Pilih Jurusan -</option>
+                                    <option value="Manajemen Informatika">Manajemen Informatika</option>
+                                    <option value="Sistem Informatika">Sistem Informatika</option>
+                                    <option value="Teknik Informatika">Teknik Informatika</option>
+                                    <option value="Sistem Komputer">Sistem Komputer</option>
+                                    <option value="Akutansi">Akutansi</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-primary" title="Simpan Data"><i class="glyphicon glyphicon-floppy-disk"></i> Simpan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </section>
 </div>
